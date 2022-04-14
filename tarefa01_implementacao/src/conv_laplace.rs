242cc53fb52b7aac4a51eb619299dca301687545
v2Kamikaze <48386792+v2Kamikaze@users.noreply.github.com>
2022-04-14 20:19:13 -0300

    Laplace incompleto

---
+++ tarefa01_implementacao/src/conv_laplace.rs
@@ -0,0 +1,64 @@
+use crate::laplace;
+use crate::matrix_utils;
+
+pub fn conv_2d_laplace(matrix: &Vec<Vec<u8>>, e: f64, padding: usize) -> Vec<Vec<(u8, u8, u8)>> {
+    let m_pixels = matrix_utils::make_pixel_matrix(matrix, padding);
+    let kernel = laplace::laplace();
+
+    let mut out = Vec::new();
+    for i in 1..(m_pixels.len() - 1) {
+        let mut line = Vec::new();
+        for j in 1..(m_pixels[i].len() - 1) {
+            let top = m_pixels[i - 1][j];
+            let bottom = m_pixels[i + 1][j];
+            let left = m_pixels[i][j - 1];
+            let right = m_pixels[i][j + 1];
+            let center = m_pixels[i][j];
+            let top_left = m_pixels[i - 1][j - 1];
+            let top_right = m_pixels[i - 1][j + 1];
+            let bottom_left = m_pixels[i + 1][j - 1];
+            let bottom_right = m_pixels[i + 1][j + 1];
+
+            let new_pixel = top_left.0 as f64 * kernel[0][0]
+                + top_left.1 as f64 * kernel[0][0]
+                + top_left.2 as f64 * kernel[0][0]
+                + top.0 as f64 * kernel[0][1]
+                + top.1 as f64 * kernel[0][1]
+                + top.2 as f64 * kernel[0][1]
+                + top_right.0 as f64 * kernel[0][2]
+                + top_right.1 as f64 * kernel[0][2]
+                + top_right.2 as f64 * kernel[0][2]
+                + left.0 as f64 * kernel[1][0]
+                + left.1 as f64 * kernel[1][0]
+                + left.2 as f64 * kernel[1][0]
+                + right.0 as f64 * kernel[1][2]
+                + right.1 as f64 * kernel[1][2]
+                + right.2 as f64 * kernel[1][2]
+                + bottom_left.0 as f64 * kernel[2][0]
+                + bottom_left.1 as f64 * kernel[2][0]
+                + bottom_left.2 as f64 * kernel[2][0]
+                + bottom.0 as f64 * kernel[2][1]
+                + bottom_left.1 as f64 * kernel[2][1]
+                + bottom_left.2 as f64 * kernel[2][1]
+                + bottom_right.0 as f64 * kernel[2][2]
+                + bottom_right.1 as f64 * kernel[2][2]
+                + bottom_right.2 as f64 * kernel[2][2]
+                + center.0 as f64 * kernel[1][1]
+                + center.1 as f64 * kernel[1][1]
+                + center.2 as f64 * kernel[1][1];
+
+        /*     let new_pixel_value = new_pixel * 1.0 / e.powf(2.0);
+
+            if new_pixel_value != 0.0 {
+                line.push((0, 0, 0));
+            } else {
+                line.push((0xFF, 0xFF, 0xFF));
+            } */
+
+            line.push((new_pixel as u8,new_pixel as u8,new_pixel as u8))
+        }
+        out.push(line);
+    }
+
+    out
+}
--- tarefa01_implementacao/src/main.rs
+++ tarefa01_implementacao/src/main.rs
@@ -1,47 +1,72 @@
 mod bmp;
 mod conv_gauss;
 mod conv_sobel;
+mod conv_laplace;
 mod gaussian;
 mod matrix_utils;
 mod sobel;
+mod laplace;
 mod utils;
 use bmp::BMPImage;
 
-// TODO: so funciona para imagens de dimensões pares, resolver isso.
+// TODO: so funciona para imagens de dimensões pares.
 
 fn main() {
     // Alg1.
-    let old_img_content = utils::get_file("./imgs/borboleta_902x526.bmp");
-    let mut image = BMPImage::new(902, 526);
+    let old_img_content = utils::get_file("src/imgs/3840_2160.bmp");
+    let mut image = BMPImage::new(3840, 2160);
     image.bmp_insert_img_data(&old_img_content[54..]);
 
-    let m = image.bmp_matrix_with_zeros_edges(1);
+/*     let m = image.bmp_matrix_with_zeros_edges(1);
+    println!("Aplicando o filtro de Gauss.");
     // Aplicando o filtro Gaussiano na matriz/imagem M.
     let m_gauss = conv_gauss::conv_2d_gauss(&m, 3, 1.0, image.bmp_image_padding());
     let m = matrix_utils::from_pixel_matrix_to_matrix(&m_gauss, image.bmp_image_padding());
     let m_gauss_linear = matrix_utils::flatenning(&m);
     image.bmp_insert_img_data(&m_gauss_linear);
-    image.write_file("./imgs/test_gauss.bmp").unwrap();
+    image.write_file("src/imgs/test_gauss.bmp").unwrap();
 
     let m = image.bmp_matrix_with_zeros_edges(1);
-
+    println!("Aplicando o filtro de Sobel no eixo Y.");
     // Aplicando Sobel na direção Y na matriz/imagem M, usando threshold de 255.0 / 2.
     let a = conv_sobel::conv_2d_sobel(&m, 3, 'y', image.bmp_image_padding());
     let a_pixels = matrix_utils::from_pixel_matrix_to_matrix(&a, image.bmp_image_padding());
     let a_linear = matrix_utils::flatenning(&a_pixels);
     image.bmp_insert_img_data(&a_linear);
-    image.write_file("./imgs/testy.bmp").unwrap();
+    image.write_file("src/imgs/testy.bmp").unwrap();
 
+    println!("Aplicando o filtro de Sobel no eixo X.");
     // Aplicando Sobel na direção X na matriz/imagem M, usando threshold de 255.0 / 2.
     let b = conv_sobel::conv_2d_sobel(&m, 3, 'x', image.bmp_image_padding());
     let b_pixels = matrix_utils::from_pixel_matrix_to_matrix(&b, image.bmp_image_padding());
     let b_linear = matrix_utils::flatenning(&b_pixels);
     image.bmp_insert_img_data(&b_linear);
-    image.write_file("./imgs/testx.bmp").unwrap();
+    image.write_file("src/imgs/testx.bmp").unwrap();
 
+    println!("Somando Sobel X e Y.");
     // Somando as duas matrizes usando => |G| = sqrt(Ay^(2) + Bx^(2)), e G = G / max(G) * 255.
     let d = conv_sobel::sobel_magnitude(&a_pixels, &b_pixels);
     let d_linear = matrix_utils::flatenning(&d);
     image.bmp_insert_img_data(&d_linear);
-    image.write_file("./imgs/test_full.bmp").unwrap();
+    image.write_file("src/imgs/test_full.bmp").unwrap();
+
+ */
+    // Alg2.
+    let m = image.bmp_matrix_with_zeros_edges(1);
+    println!("Aplicando o filtro de Gauss.");
+    // Aplicando o filtro Gaussiano na matriz/imagem M.
+    let m_gauss = conv_gauss::conv_2d_gauss(&m, 3, 1.6, image.bmp_image_padding());
+    let m = matrix_utils::from_pixel_matrix_to_matrix(&m_gauss, image.bmp_image_padding());
+    let m_gauss_linear = matrix_utils::flatenning(&m);
+    image.bmp_insert_img_data(&m_gauss_linear);
+    image.write_file("./src/imgs/test_gauss.bmp").unwrap();
+
+    let m = image.bmp_matrix_with_zeros_edges(1);
+
+    println!("Aplicando o filtro de Laplace.");
+    let a = conv_laplace::conv_2d_laplace(&m, 0.0001, image.bmp_image_padding());
+    let a_pixels = matrix_utils::from_pixel_matrix_to_matrix(&a, image.bmp_image_padding());
+    let a_linear = matrix_utils::flatenning(&a_pixels);
+    image.bmp_insert_img_data(&a_linear);
+    image.write_file("./src/imgs/test_laplace.bmp").unwrap();
 }
