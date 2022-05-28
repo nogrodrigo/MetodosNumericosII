e4f88322315d039a3c8a0ebef0ecddbfb4a2027d
v2Kamikaze <48386792+v2Kamikaze@users.noreply.github.com>
2022-05-28 17:48:08 -0300

    Funciona agora

--- tarefa07b/exp.py
+++ tarefa07b/exp.py
@@ -2,21 +2,43 @@ from math import exp, tanh, cosh, sinh, pi, inf
 from typing import Callable, Tuple
 from colors import Colors
 
+def gauss_hermite_4(xi: float, xf: float, func: Callable[[float], float]) -> float:
+    x1 = - ((3/2) - ((6)**0.5)/2)**0.5
+    x2 = - ((3/2) + ((6)**0.5)/2)**0.5
+    x3 = ((3/2) - ((6)**0.5)/2)**0.5
+    x4 = ((3/2) + ((6)**0.5)/2)**0.5
+
+    w1 = 0.8049140
+    w2 = 0.0813128
+    w3 = 0.8049140
+    w4 = 0.0813128
+
+    return (exp(x1 * x1) * f_bar_simp(xi, xf, x1, func) * w1) + exp(x2 * x2) *(f_bar_simp(xi, xf, x2, func) * w2) + exp(x3 * x3) * (f_bar_simp(xi, xf, x3, func) * w3) + exp(x4 * x4) * (f_bar_simp(xi, xf, x4, func)*w4)
 
 
-def gauss_legendre_3(xi: float | int, xf: float | int, func: Callable[[int | float], float]):
+
+def gauss_legendre_simp_2(xi: float | int, xf: float | int, ci: float | int, cf: float | int, func: Callable[[float], float]):
+    w1 = 1
+    w2 = 1
+    xs_1 = ((xi + xf) / 2) + ((xf - xi) / 2) * (- (1/3)**0.5)
+    xs_2 = ((xi + xf) / 2) + ((xf - xi) / 2) * ((1/3)**0.5)
+    func_xs_1 = f_bar_simp(xi, xf, s_bar(ci, cf, xs_1), func)
+    func_xs_2 = f_bar_simp(xi, xf, s_bar(ci, cf, xs_2), func)
+    return ((cf - ci) / 2) * (func_xs_1 * w1 + func_xs_2 * w2)
+
+def gauss_legendre_simp_3(xi: float | int, xf: float | int, ci: float | int, cf: float | int, func: Callable[[int | float], float]):
     w1 = w3 = 5 / 9
     w2 = 8 / 9
     xs_1 = ((xi + xf) / 2) + ((xf - xi) / 2) * (- (3/5)**0.5)
     xs_2 = ((xi + xf) / 2) + ((xf - xi) / 2) * 0
     xs_3 = ((xi + xf) / 2) + ((xf - xi) / 2) * ((3/5)**0.5)
-    func_xs_1 = f_bar_simp(xi, xf, xs_1, func)
-    func_xs_2 = f_bar_simp(xi, xf, xs_2, func)
-    func_xs_3 = f_bar_simp(xi, xf, xs_3, func)
-    return ((xf - xi) / 2) * (func_xs_1 * w1 + func_xs_2 * w2 + func_xs_3 * w3)
+    func_xs_1 = f_bar_simp(xi, xf, s_bar(ci, cf, xs_1), func)
+    func_xs_2 = f_bar_simp(xi, xf, s_bar(ci, cf, xs_2), func)
+    func_xs_3 = f_bar_simp(xi, xf, s_bar(ci, cf, xs_3), func)
+    return ((cf - ci) / 2) * (func_xs_1 * w1 + func_xs_2 * w2 + func_xs_3 * w3)
 
 
-def gauss_legendre_4(xi: float | int, xf: float | int, ci, cf, func: Callable[[int | float], float]):
+def gauss_legendre_simp_4(xi: float | int, xf: float | int, ci: float | int, cf: float | int, func: Callable[[int | float], float]):
     w1 = w4 = (18 + 30**0.5) / 36
     w2 = w3 = (18 - 30**0.5) / 36
     r1 = ((3 / 7) + (2 / 7) * (6 / 5)**0.5)**0.5
@@ -30,6 +52,17 @@ def gauss_legendre_4(xi: float | int, xf: float | int, ci, cf, func: Callable[[i
     func_xs_3 = f_bar_simp(xi, xf, s_bar(ci, cf, xs_3), func)
     func_xs_4 = f_bar_simp(xi, xf, s_bar(ci, cf, xs_4), func)
     return ((cf - ci) / 2) * (func_xs_1 * w1 + func_xs_2 * w2 + func_xs_3 * w3 + func_xs_4 * w4)
+
+
+
+def gauss_legendre_integrate(xi: float | int, xf: float | int, ci: float | int, cf: float | int, func: Callable[[int | float], float], num_p: int) -> float:
+    if num_p == 2:
+        return gauss_legendre_simp_2(xi, xf, ci, cf, func)
+    elif num_p == 3:
+        return gauss_legendre_simp_3(xi, xf, ci, cf, func)
+    elif num_p == 4:
+        return gauss_legendre_simp_4(xi, xf, ci, cf, func)
+    
 
 # ============================================================================================================== #
 # ================================================== SIMPLES =================================================== #
@@ -61,11 +94,11 @@ def s_bar(ini: float | int, fin: float | int, s: float | int) -> float:
 
 
 def xs_simp(a: float | int, b: float | int, s: float | int) -> float:
-    return ((a + b) / 2) + (((b - a) / 2.0) * tanh(s))
+    return ((a + b) / 2) + ((b - a) / 2.0) * tanh(s)
 
 
 def ds_simp(a: float | int, b: float | int, s: float | int) -> float:
-    return ((b - a) / 2) * (1 / (cosh(s)) ** 2)
+    return ((b - a) / 2) * (1 / (cosh(s) ** 2))
 
 
 def f_bar_simp(ini: float, fin: float, s: float, f: Callable[[float], float]) -> float:
@@ -75,30 +108,29 @@ def f_bar(ini: float, fin: float, s: float, f: Callable[[float], float]) -> floa
     return f(xs_simp(ini, fin, s)) * ds_simp(ini, fin, s)
 
 
-def gauss_legendre_2(xi: float | int, xf: float | int, ci: float | int, cf: float | int, func: Callable[[float], float]):
-    w1 = 1
-    w2 = 1
-    xs_1 = ((xi + xf) / 2) + ((xf - xi) / 2) * (- (1/3)**0.5)
-    xs_2 = ((xi + xf) / 2) + ((xf - xi) / 2) * ((1/3)**0.5)
-    func_xs_1 = f_bar_simp(xi, xf, s_bar(ci, cf, xs_1), func)
-    func_xs_2 = f_bar_simp(xi, xf, s_bar(ci, cf, xs_2), func)
-    return ((cf - ci) / 2) * (func_xs_1 * w1 + func_xs_2 * w2)
 
 
 
+def gauss_legendre_exp_simp_calculate(xi: float, xf: float, ci: float, cf: float, func:  Callable[[float], float], num_p: int,  eps: float) -> float:
+    new_i = inf
+    old_i = inf
+    n = 1
+    err = inf
 
-def gauss_hermite_4(xi: float, xf: float, func: Callable[[float], float]) -> float:
-    x1 = - ((3/2) - ((6)**0.5)/2)**0.5
-    x2 = - ((3/2) + ((6)**0.5)/2)**0.5
-    x3 = ((3/2) - ((6)**0.5)/2)**0.5
-    x4 = ((3/2) + ((6)**0.5)/2)**0.5
+    while err > eps:
+        old_i = new_i
+        delta_x = (cf - ci) / n
+        new_i = 0
 
-    w1 = 0.8049140
-    w2 = 0.0813128
-    w3 = 0.8049140
-    w4 = 0.0813128
+        for i in range(n):
+            x_ini = ci + (i * delta_x)
+            x_fin = x_ini + delta_x
+            new_i += gauss_legendre_integrate(xi, xf, x_ini, x_fin, func, num_p)
 
-    return (exp(x1 * x1) * f_bar_simp(xi, xf, x1, func) * w1) + exp(x2 * x2) *(f_bar_simp(xi, xf, x2, func) * w2) + exp(x3 * x3) * (f_bar_simp(xi, xf, x3, func) * w3) + exp(x4 * x4) * (f_bar_simp(xi, xf, x4, func)*w4)
+        err = abs((new_i - old_i) / new_i)
+        n *= 4
+
+    return new_i
 
 
 # ============================================================================================================== #
--- tarefa07b/main.py
+++ tarefa07b/main.py
@@ -1,9 +1,22 @@
 from exp import *
 
 
-print(gauss_hermite_4(-1, 1, lambda x: (1/(x**2)**(1/3)))) # OK
-print(gauss_legendre_4(-1, 1, -1.2, 1.2, lambda x: (1/(x**2)**(1/3)))) # NO
+print("Eq 0")
+# print("Hermite: ")
+# print(gauss_hermite_4(0, 1, lambda x: (1/(x)**0.5))) # OK
+print("Legendre: ")
+print(gauss_legendre_exp_simp_calculate(0, 1, -6, 6, lambda x: (1/(x)**0.5), num_p=4, eps=0.01)) # OK
+print("==============================================")
 
+print("Eq 1")
+# print("Hermite: ")
+# print(gauss_hermite_4(-1, 1, lambda x: (1/(x**2)**(1/3)))) # OK
+print("Legendre: ")
+print(gauss_legendre_exp_simp_calculate(-1, 1, -5, 5, lambda x: (1/(x**2)**(1/3)), num_p=4, eps=0.01)) # OK
+print("==============================================")
 
-print(gauss_hermite_4(-2, 0, lambda x: (1/(4 - x**2)**(1/2)))) # OK
-print(gauss_legendre_2(-2, 0, -5, 5, lambda x: (1/(4 - x**2)**(1/2)))) # NO
+print("Eq 2")
+# print("Hermite: ")
+# print(gauss_hermite_4(-2, 0, lambda x: (1/(4 - x**2)**(1/2)))) # OK
+print("Legendre: ")
+print(gauss_legendre_exp_simp_calculate(-2, 0, -4, 4, lambda x: (1/(4 - x**2)**(1/2)), num_p=4, eps=0.01)) # OK
