acb6c95587e695d082fe0be9a3a454ee1861741c
v2Kamikaze <48386792+v2Kamikaze@users.noreply.github.com>
2022-07-10 04:51:13 -0300

    Adams-Bashforth implementado, falta apenas aplicar.

--- tarefa13/runge_kutta.py
+++ tarefa13/runge_kutta.py
@@ -9,19 +9,28 @@ k = 0.25
 m = 2
 g = 10
 
-def getSubstep(pastState: Tuple[float, float], dt: float, j: int) -> Tuple[float, float]:
+
+def getSubstep(
+    pastState: Tuple[float, float], dt: float, j: int
+) -> Tuple[float, float]:
     vPast, yPast = pastState
     v = vPast + dt * (-g - k * vPast / m) / j
     y = yPast + dt * (vPast) / j
-    return (v,y)
+    return (v, y)
+
 
 def getNextState(pastState: Tuple[float, float], dt: float) -> Tuple[float, float]:
     vPast, yPast = pastState
     subv1, suby1 = getSubstep(pastState, dt, 2)
     subv2, suby2 = getSubstep(pastState, dt, 1)
-    v = vPast + dt * ((-g - k * vPast / m) / 6 + 4 * (-g - k * subv1 / m) / 6 + (-g - k * subv2 / m) / 6)
+    v = vPast + dt * (
+        (-g - k * vPast / m) / 6
+        + 4 * (-g - k * subv1 / m) / 6
+        + (-g - k * subv2 / m) / 6
+    )
     y = yPast + dt * (vPast / 6 + 4 * subv1 / 6 + subv2 / 6)
-    return (v,y)
+    return (v, y)
+
 
 for dt in [0.1, 0.01, 0.001, 0.0001]:
     ymax = y_0
@@ -38,4 +47,14 @@ for dt in [0.1, 0.01, 0.001, 0.0001]:
             maxHeightTime = t_0 + dt * iter
         if y <= 0:
             time += dt * iter
-    print("ymax:                \t"+str(ymax)+"\nTempo até ymax: \t"+str(maxHeightTime)+"\nVelocidade no Impacto: \t"+str(v)+"\nTempo até o Impacto: \t"+str(time)+"\n")
+    print(
+        "ymax:                \t"
+        + str(ymax)
+        + "\nTempo até ymax: \t"
+        + str(maxHeightTime)
+        + "\nVelocidade no Impacto: \t"
+        + str(v)
+        + "\nTempo até o Impacto: \t"
+        + str(time)
+        + "\n"
+    )
---
+++ tarefa14/main.py
@@ -0,0 +1,90 @@
+# Usando como modelo os passos de desenvolvimento do método preditor-corretor de
+# terceira ordem, desenvolva o método preditor-corretor de quarta ordem.
+# Si+1 = Si + dt/24 * ( 55*Fi - 59*Fi-1 + 37*Fi-2 - 9*Fi-3 ) <- Predição
+# Si+1 = Si + dt/24 * ( 9*Fi+1 + 19*Fi - 5*Fi-1 + Fi-2 ) <- Correção
+from typing import Callable, Tuple
+
+
+t_0 = 0
+v_0 = 5
+y_0 = 200
+k = 0.25
+m = 2
+g = 10
+dt = 0.2
+
+
+F = lambda v_t, y: (-g - (k / m) * v_t)
+
+
+# t = x, v = y
+def adams_bashforth(v: float, x_0: float, y_0: float, dt: float, F: Callable) -> float:
+    y_1, y_2, y_3 = RK4(x_0, y_0, dt, F)
+
+    _y_0 = F(x_0, y_0)
+    x_1 = x_0 + dt
+    _y_1 = F(x_1, y_1)
+    x_2 = x_1 + dt
+    _y_2 = F(x_2, y_2)
+    x_3 = x_2 + dt
+    _y_3 = F(x_3, y_3)
+    print("Valores de Y' calculados: ", _y_0, _y_1, y_2, _y_3)
+    # Predição
+    _y_4 = y_3 + ((dt / 24) * ((55 * _y_3) - (59 * _y_2) + (37 * _y_1) - (9 * _y_0)))
+    print("Predição: ", _y_4)
+    # Recalculando y'4 com o valor encontrado na predição.
+    # y'4 = F(v, y'4)
+    #         ^-------- O valor que estamos tentando aproximar.
+    _y_4 = F(v, _y_4)
+    # Correção
+    y_4 = y_3 + ((dt / 24) * (9 * _y_4 + 19 * _y_3 - 5 * _y_2 + _y_1))
+    print("Correção: ", y_4)
+    return y_4
+
+
+def RK4(x_0: float, y_0: float, dt: float, F: Callable) -> Tuple[float]:
+    k1 = K(y_0, x_0, F)
+    x_0 += dt
+    k2 = K(k1, x_0, F)
+    x_0 += dt
+    k3 = K(k2, x_0, F)
+    return k1, k2, k3
+
+
+def K(curr_y: float, x_0: float, F: Callable) -> float:
+    k1 = F(x_0, curr_y)
+    k2 = F(x_0 + (dt / 2), curr_y + (dt * k1) / 2)
+    k3 = F(x_0 + (dt / 2), curr_y + (dt * k2) / 2)
+    k4 = F(x_0 + dt, curr_y + dt * k3)
+    return curr_y + (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+
+
+adams_bashforth(0.8, 0, 1, dt, F)
+
+
+""" for dt in [0.1, 0.01, 0.001, 0.0001]:
+    ymax = y_0
+    maxHeightTime = 0
+    time = 0
+    y = y_0
+    v = v_0
+    iter = 0
+    while y > 0:
+        iter += 1
+        v, y = getNextState((v, y), dt)
+        if y > ymax:
+            ymax = y
+            maxHeightTime = t_0 + dt * iter
+        if y <= 0:
+            time += dt * iter
+    print(
+        "ymax:                \t"
+        + str(ymax)
+        + "\nTempo até ymax: \t"
+        + str(maxHeightTime)
+        + "\nVelocidade no Impacto: \t"
+        + str(v)
+        + "\nTempo até o Impacto: \t"
+        + str(time)
+        + "\n"
+    ) """
