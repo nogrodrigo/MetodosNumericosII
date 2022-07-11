d7a8bc15d0043193c7185b5efe78a5f553e11b66
v2Kamikaze <48386792+v2Kamikaze@users.noreply.github.com>
2022-07-10 23:44:37 -0300

    Velocidade OK

--- tarefa14/adams_bashforth.py
+++ tarefa14/adams_bashforth.py
@@ -12,20 +12,24 @@ y_0 = 200
 k = 0.25
 m = 2
 g = 10
-dt = 0.2
+dt = 0.01
 
 
-F = lambda v, _: -g - (k / m) * v
+""" def adams_bashforth(
+    k1: float,
+    k2: float,
+    k3: float,
+    k4: float,
+    dt: float,
+    F: Callable,
+) -> float:
+    x_0 = 0
 
-
-def adams_bashforth(v: float, x_0: float, y_0: float, dt: float, F: Callable) -> float:
-    y_1, y_2, y_3 = RK4(x_0, y_0, dt, F)
-
-    _y_0 = F(x_0, y_0)
+    _y_0 = F(x_0, k1)
     x_1 = x_0 + dt
-    _y_1 = F(x_1, y_1)
+    _y_1 = F(x_1, k2)
     x_2 = x_1 + dt
-    _y_2 = F(x_2, y_2)
+    _y_2 = F(x_2, k3)
     x_3 = x_2 + dt
     _y_3 = F(x_3, y_3)
     print("Valores de Y' calculados: ", _y_0, _y_1, y_2, _y_3)
@@ -35,14 +39,15 @@ def adams_bashforth(v: float, x_0: float, y_0: float, dt: float, F: Callable) ->
     # Recalculando y'4 com o valor encontrado na predição.
     # y'4 = F(v, y'4)
     #         ^-------- O valor que estamos tentando aproximar.
-    _y_4 = F(v, _y_4)
+    # _y_4 = F(v, _y_4)
     # Correção
     y_4 = y_3 + ((dt / 24) * (9 * _y_4 + 19 * _y_3 - 5 * _y_2 + _y_1))
     print("Correção de Y...........: ", y_4)
     print("Valor de X..............: ", x_3)
-    return x_3, y_4
+    return x_3, y_4 """
 
 
+""" 
 def RK4(x_0: float, y_0: float, dt: float, F: Callable) -> Tuple[float]:
     k1 = K(y_0, x_0, F)
     x_0 += dt
@@ -50,17 +55,46 @@ def RK4(x_0: float, y_0: float, dt: float, F: Callable) -> Tuple[float]:
     x_0 += dt
     k3 = K(k2, x_0, F)
     return k1, k2, k3
+ """
 
 
-def K(curr_y: float, x_0: float, F: Callable) -> float:
-    k1 = F(x_0, curr_y)
-    k2 = F(x_0 + (dt / 2), curr_y + (dt * k1) / 2)
-    k3 = F(x_0 + (dt / 2), curr_y + (dt * k2) / 2)
-    k4 = F(x_0 + dt, curr_y + dt * k3)
-    return curr_y + (dt / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+def sub_step(
+    last_state: Tuple[float, float],
+    dt: float,
+) -> Tuple[Tuple[float, float, float, float]]:
+    last_v, last_y = last_state
+
+    v1 = -g - (k / m) * last_v
+    v2 = -g - (k / m) * (last_v + (dt / 2))
+    v3 = -g - (k / m) * (last_v + (dt / 2))
+    v4 = -g - (k / m) * (last_v + dt)
+    v = last_v + (dt / 6) * (v1 + 2 * v2 + 2 * v3 + v4)
+    print("Velocidade tá OK!")
+    print("v: ", v1, v2, v3, v4)
+    print("last_v: ", v)
+
+    y1 = last_y
+    y2 = last_y + (dt * last_v) / 2
+    y3 = last_y + (dt * last_v) / 2
+    y4 = last_y + (dt * last_v)
+    print(last_y)
+    y = last_y + ((dt / 6) * (y1 + (2 * y2) + (2 * y3) + y4))
+    print("Altura tá ERRADA!")
+    print("y: ", y1, y2, y3, y4)
+    print("last_y: ", y)
+
+    return (v2, v3, v4, v), (y2, y3, y4, y)
 
 
-print("V0......................:", v_0)
-print("Y0......................:", y_0)
-print("T0......................:", t_0)
-adams_bashforth(0, v_0, y_0, dt, F)
+def getSubstep(
+    pastState: Tuple[float, float], dt: float, j: int
+) -> Tuple[float, float]:
+    vPast, yPast = pastState
+    v = vPast + dt * (-g - k * vPast / m) / j
+    y = yPast + dt * (vPast) / j
+    print("v: ", v, "\ny: ", y)
+    return (v, y)
+
+
+sub_step((v_0, y_0), dt)
+getSubstep((v_0, y_0), dt, 1)
