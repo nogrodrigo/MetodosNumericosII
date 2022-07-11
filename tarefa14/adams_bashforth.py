d9078c6705e67382d1ade76201c709d136172c59
nogrodrigo <thenogrodrigo1@gmail.com>
2022-07-11 13:54:46 -0300

    Fix tarefa14

--- tarefa14/adams_bashforth.py
+++ tarefa14/adams_bashforth.py
@@ -65,19 +65,67 @@ def next_state(s1, s2, s3, s4, dt: float):
     _v2 = -g - (k / m) * v2
     _v3 = -g - (k / m) * v3
     _v4 = -g - (k / m) * v4
-    _v5 = v4 + ((dt / 24) * ((55 * _v4) - (59 * _v3) + (37 * _v2) - (9 * _v1)))
-    _v5 = v4 + ((dt / 24) * (9 * _v5 + 19 * _v4 - 5 * _v3 + _v2))
 
-    _y1 = y1
-    _y2 = y2
-    _y3 = y3
-    _y4 = y4
-    _y5 = y4 + ((dt / 24) * ((55 * _y4) - (59 * _y3) + (37 * _y2) - (9 * _y1)))
+    _y1 = v1
+    _y2 = v2
+    _y3 = v3
+    _y4 = v4
+
+    # Predição
+    v5 = v4 + ((dt / 24) * ((55 * _v4) - (59 * _v3) + (37 * _v2) - (9 * _v1)))
+    y5 = y4 + ((dt / 24) * ((55 * _y4) - (59 * _y3) + (37 * _y2) - (9 * _y1)))
+
+    _v5 = -g - (k / m) * v5
+    _y5 = v5
+
+    #Correção
+    _v5 = v4 + ((dt / 24) * (9 * _v5 + 19 * _v4 - 5 * _v3 + _v2))
     _y5 = y4 + ((dt / 24) * (9 * _y5 + 19 * _y4 - 5 * _y3 + _y2))
 
     return (_v5, _y5)
 
 
+v_1, y_1 = v_0 + (dt / 2) * (- g - k * v_0 / m), y_0 + (dt / 2) * v_0
+v_2, y_2 = v_1 + (dt / 2) * (- g - k * v_1 / m), y_1 + (dt / 2) * v_1
+v_3, y_3 = v_2 + dt * (- g - k * v_2 / m),       y_2 + dt * v_2
+
+s_0 = (v_0, y_0)
+s_1 = (v_1, y_1)
+s_2 = (v_2, y_2)
+s_3 = (v_3, y_3)
+
+for dt in [0.1, 0.01, 0.001, 0.0001]:
+
+    ymax = y_0
+    maxHeightTime = 0
+    time = 0
+    iter = 0
+    v = v_0
+    y = y_0
+    s0, s1, s2, s3 = s_0, s_1, s_2, s_3
+    while y > 0:
+        iter += 1
+        s4 = next_state(s0, s1, s2, s3, dt)
+        s0, s1, s2, s3 = s1, s2, s3, s4
+        v, y = s4
+        if y > ymax:
+            ymax = y
+            maxHeightTime = t_0 + dt * iter
+        if y <= 0:
+            time += dt * iter
+    print(
+        "ymax:                \t"
+        + str(ymax)
+        + "\nTempo até ymax: \t"
+        + str(maxHeightTime)
+        + "\nVelocidade no Impacto: \t"
+        + str(v)
+        + "\nTempo até o Impacto: \t"
+        + str(time)
+        + "\n"
+    )
+
+"""
 s0 = (v_0, y_0)
 s1 = sub_step(s0, dt)
 s2 = sub_step(s1, dt)
@@ -88,7 +136,7 @@ _s1 = getSubstep(s0, dt, 1)
 _s2 = getSubstep(_s1, dt, 1)
 _s3 = getSubstep(_s2, dt, 1)
 print(s0, _s1, _s2, _s3)
-
+"""
 """ s4 = next_state(s0, s1, s2, s3, dt)
 
 s0, s1, s2, s3 = s1, s2, s3, s4
